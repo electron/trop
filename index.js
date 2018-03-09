112b103dc26a8fad2040ed23d69bbc4817c22d50
Shelley Vohr <shelley.vohr@gmail.com>
2018-03-09 14:35:12 -0600

    add config-file-changed first pass

--- index.js
+++ index.js
@@ -1,11 +1,43 @@
+const randomColor = require('randomcolor')
+
 module.exports = (robot) => {
-  robot.log('Yay, the app was loaded!')
-  robot.on('config-file-changed', (context) => {
-    // get "watched project boards"
-    // for each column, make a label
+  // get watched board and create labels based on column names
+  robot.on('config-file-changed', async context => {
+    const config = await context.config('config.yml')
+
+    const data = {
+      owner: context.payload.repository.owner.login,
+      repo: context.payload.repository.name
+    }
+
+    const id = config['watchedProject']['id']
+    const columns = await context.projects.getProjectColumns({id})
+
+    // generate labels based on project column names
+    columns.forEach(async column => {
+      // check if label already exists
+      const label = await context.issues.getLabel({
+        owner: data.owner,
+        repo: data.repo,
+        name: column.name
+      })
+
+      // if it doesn't, create a new label
+      if (!label) {
+        const newLabel = context.issues.createLabel({
+          owner: data.owner,
+          repo: data.repo,
+          name: column.name,
+          color: randomColor()
+        })
+      }
+    })
+
+    robot.log('all labels created!')
   })
-  robot.on('issues.labeled', (context) => {
-    // see if any label relates to a column in a watch board
+
+  robot.on('issues.labeled', async context => {
+    // see if any label relates to a column in the watch board
     // add issue to column
   })
 }
