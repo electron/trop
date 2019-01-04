35b951673009be7f57acbf5ebf38c29c308b5ef4
Shelley Vohr <codebytere@github.com>
2019-01-04 10:27:26 -0800

    feat: allow trop to label manual backports (#76)
    * feat: allow trop to label manual backports
    
    * fix: ensure label exists before attempting deletion

--- src/backport/utils.ts
+++ src/backport/utils.ts
@@ -17,6 +17,7 @@ const { parse: parseDiff } = require('what-the-diff');
 const TARGET_LABEL_PREFIX = 'target/';
 const MERGED_LABEL_PREFIX = 'merged/';
 const IN_FLIGHT_LABEL_PREFIX = 'in-flight/';
+const NEEDS_MANUAL_LABEL_PREFIX = 'needs-manual-bp/';
 
 export const labelToTargetBranch = (label: Label, prefix: string) => {
   return label.name.replace(prefix, '');
@@ -336,6 +337,22 @@ export const backportImpl = async (robot: Application,
           body: `I was unable to backport this PR to "${targetBranch}" cleanly;
    you will need to perform this backport manually.`,
         }) as any);
+
+        const labelPrefixes = await getLabelPrefixes(context);
+
+        const labelToRemove = labelPrefixes.target + targetBranch;
+        if (labelExistsOnPR(context, labelToRemove)) {
+          await context.github.issues.removeLabel(context.repo({
+            number: pr.number,
+            name: labelToRemove,
+          }));
+        }
+
+        const labelToAdd = labelPrefixes.needsManual + targetBranch;
+        await context.github.issues.addLabels(context.repo({
+          number: pr.number,
+          labels: [labelToAdd],
+        }));
       }
 
       if (purpose === BackportPurpose.Check) {
@@ -367,12 +384,28 @@ export const backportImpl = async (robot: Application,
   );
 };
 
+const labelExistsOnPR = async (context: Context, labelName: string) => {
+  const base = context.payload.pull_request.base;
+
+  const labels = await context.github.issues.getIssueLabels(context.repo({
+    owner: base.repo.owner.login,
+    repo: base.repo.name,
+    number: context.payload.pull_request.number,
+    per_page: 100,
+    page: 1,
+  }));
+
+  return labels.data.some(label => label.name === labelName);
+};
+
 export const getLabelPrefixes = async (context: Pick<Context, 'config'>) => {
   const config = await context.config<TropConfig>('config.yml') || {};
   const target = config.targetLabelPrefix || TARGET_LABEL_PREFIX;
   const inFlight = config.inFlightLabelPrefix || IN_FLIGHT_LABEL_PREFIX;
   const merged = config.mergedLabelPrefix || MERGED_LABEL_PREFIX;
-  return { target, inFlight, merged };
+  const needsManual = config.needsManualLabelPrefix || NEEDS_MANUAL_LABEL_PREFIX;
+
+  return { target, inFlight, merged, needsManual };
 };
 
 export const backportToLabel = async (
