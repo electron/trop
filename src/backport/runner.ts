81d03e5e56e70e0d1375ed997f7257602e7beeda
Samuel Attard <samuel.r.attard@gmail.com>
2019-02-08 13:10:37 -0800

    feat: support authenticated clones (#82)

--- src/backport/runner.ts
+++ src/backport/runner.ts
@@ -50,10 +50,17 @@ export const initRepo = async (options: InitRepoOptions) => {
   await fs.remove(dir);
   await fs.mkdirp(dir);
   const git = simpleGit(dir);
-  await git.clone(
-    `https://github.com/${slug}.git`,
-    '.',
-  );
+  if (process.env.GITHUB_FORK_USER_CLONE_LOGIN) {
+    await git.clone(
+      `https://${process.env.GITHUB_FORK_USER_CLONE_LOGIN}:${process.env.GITHUB_FORK_USER_TOKEN}@github.com/${slug}.git`,
+      '.',
+    );
+  } else {
+    await git.clone(
+      `https://github.com/${slug}.git`,
+      '.',
+    );
+  }
 
   // Clean up just in case
   await git.reset('hard');
