a6f46930e9c61f881643577c80bcd04ad3597fa9
Shelley Vohr <shelley.vohr@gmail.com>
2020-06-30 09:52:09 -0700

    feat: automatically label semver-minor pr backports (#157)

--- src/constants.ts
+++ src/constants.ts
@@ -4,3 +4,6 @@ export const NUM_SUPPORTED_VERSIONS = process.env.NUM_SUPPORTED_VERSIONS || 4;
 
 export const SKIP_CHECK_LABEL =
   process.env.SKIP_CHECK_LABEL || 'backport-check-skip';
+
+export const BACKPORT_REQUESTED_LABEL =
+  process.env.BACKPORT_REQUESTED_LABEL || 'backport/requested 🗳';
