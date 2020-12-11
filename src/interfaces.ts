29562efce269040764a2daa1367e1603cadec2c5
Shelley Vohr <shelley.vohr@gmail.com>
2020-12-10 20:23:20 -0800

    feat: allow users with write access to backport

--- src/interfaces.ts
+++ src/interfaces.ts
@@ -1,11 +1,3 @@
-export interface TropConfig {
-  targetLabelPrefix?: string;
-  inFlightLabelPrefix?: string;
-  mergedLabelPrefix?: string;
-  needsManualLabelPrefix?: string;
-  authorizedUsers?: string[];
-}
-
 export interface RemotesOptions {
   dir: string;
   remotes: {
