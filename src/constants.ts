55615207c16c69abaa89b0493ed42703c675a110
Shelley Vohr <shelley.vohr@gmail.com>
2020-12-16 13:14:23 -0800

    fix: label manual backports with semver labels (#177)

--- src/constants.ts
+++ src/constants.ts
@@ -2,6 +2,10 @@ export const CHECK_PREFIX = 'Backportable? - ';
 
 export const NUM_SUPPORTED_VERSIONS = process.env.NUM_SUPPORTED_VERSIONS || 4;
 
+export const BACKPORT_LABEL = 'backport';
+
+export const SEMVER_PREFIX = 'semver/';
+
 export const SKIP_CHECK_LABEL =
   process.env.SKIP_CHECK_LABEL || 'backport-check-skip';
 
